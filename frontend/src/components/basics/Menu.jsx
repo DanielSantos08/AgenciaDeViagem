e6071f6c7d7358c011af364a534662e98dce1b6e
Daniel Santos <89922562+DanielSantos08@users.noreply.github.com>
2022-03-15 15:58:20 -0300

    tela de perfil

--- frontend/src/components/basics/Menu.jsx
+++ frontend/src/components/basics/Menu.jsx
@@ -28,6 +28,9 @@ export default function Menu() {
                          <li className="nav-item btn btn-primary">
                          <Link to="/Cadastro" className="nav-link text-white">Cadastre-se</Link>
                          </li>
+                         <li className="nav-item btn btn-primary">
+                         <Link to="/Cliente" className="nav-link text-white">Perfil</Link>
+                         </li>
                      </ul>
                  </div>
              </div>
